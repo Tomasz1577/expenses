3216fac20f660a1930e89beb1b2aba2486568c6d
Tomasz <tomasz1577@gmail.com>
2022-10-05 20:02:30 +0200

    Task 1/7

--- expenses/forms.py
+++ expenses/forms.py
@@ -3,10 +3,15 @@ from .models import Expense
 
 
 class ExpenseSearchForm(forms.ModelForm):
+    start_date = forms.DateField(widget=forms.NumberInput(attrs={'type': 'date'}))
+    end_date = forms.DateField(widget=forms.NumberInput(attrs={'type': 'date'}))
+
     class Meta:
         model = Expense
-        fields = ('name',)
+        fields = ('name', 'start_date', 'end_date')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+        self.fields['start_date'].required = False
+        self.fields['end_date'].required = False
--- expenses/views.py
+++ expenses/views.py
@@ -7,7 +7,8 @@ from .reports import summary_per_category
 
 class ExpenseListView(ListView):
     model = Expense
-    paginate_by = 5
+    data = Expense
+    paginate_by = 10
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
@@ -15,8 +16,20 @@ class ExpenseListView(ListView):
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
+            start_date = form.cleaned_data.get('start_date')
+            end_date = form.cleaned_data.get('end_date')
+
+
             if name:
                 queryset = queryset.filter(name__icontains=name)
+
+            if start_date:
+                queryset = queryset.filter(date__gt=start_date)
+            if end_date:
+                queryset = queryset.filter(date__lt=end_date)
+
+
+
 
         return super().get_context_data(
             form=form,
